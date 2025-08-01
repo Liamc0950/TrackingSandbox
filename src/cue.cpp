403ab0dd22200a37cd8e7817f6f382994c97bbbb
Liam Corley <Liamc0950@gmail.com>
2025-08-01 19:31:54 -0400

    Building out Cue

--- src/cue.cpp
+++ src/cue.cpp
@@ -4,12 +4,13 @@
 class Cue
 {
   private:
-    unsigned short m_number;
-	  std::vector<MoveInstruction*>* m_move_instructions;
+	std::vector<MoveInstruction*>* m_move_instructions;
 
   public:
-	  Cue(int number);
-    
+	Cue(int number){
+      m_number = number;
+    }
+
     void process_channel_values();
 
     void UpdateCueOnly(std::vector<MoveInstruction>);
@@ -17,10 +18,6 @@ class Cue
     void UpdateTrace(std::vector<MoveInstruction>);
     void UpdateTraceTrace(std::vector<MoveInstruction>);
 
-    unsigned short get_number();
-
     std::vector<MoveInstruction> get_move_instructions();
-
-
 
 };
